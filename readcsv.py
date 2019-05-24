9ad8bc7a1428531217c2a7835895370318464ba8
杜甲 <>
2019-05-24 19:15:28 +0800

    将csv转json新增

--- readcsv.py
+++ readcsv.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import json
 
 # csv_file = csv.reader(open('/Users/dujia/mswork/js/jst/datasets.csv','rU'))
 # print(csv_file)
@@ -10,8 +11,14 @@ cwd = os.getcwd()
 print("Current folder is %s" % (cwd))
 
 csvfile = open('/Users/dujia/mswork/js/jst/datasets.csv', 'r')
-reader = [each for each in csv.DictReader(csvfile, delimiter='\t')]  # 这里设置分号为分隔符
 
+reader = [each for each in csv.DictReader(csvfile, delimiter='\t')]  # 这里设置分号为分隔符
+j = json.dumps(reader)
+
+file_handle = open('/Users/dujia/PycharmProjects/mskjjs/1.json', mode='w')
+file_handle.write(j)
+
+print(j)
 for row in reader:
     print(row['S_INFO_COMPNAME'] + ' ' + row['B_INFO_CREDITRATING'] + ' ' + row['REPORT_PERIODB'])
 csvfile.close()
